edff74c6ecfc9f520b2abb946b347451d5bcfe29
ayush <ayushgupta220503@gmail.com>
2022-08-20 11:31:35 +0530

    horizontal scroll bar completed

--- Chapter 3 Activites and Layout/ScrollApp/app/src/main/java/com/example/scrollapp/MainActivity.java
+++ Chapter 3 Activites and Layout/ScrollApp/app/src/main/java/com/example/scrollapp/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.scrollapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity{
+    Button button = findViewById(R.id.button);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 } 
