6547d1bc1f6fae422781290c9535cab6e3065ed4
ayush <ayushgupta220503@gmail.com>
2022-08-20 12:09:44 +0530

    log.d(tag and msg) not working 😢😢

--- Chapter 3 Activites and Layout/ScrollApp/app/src/main/java/com/example/scrollapp/MainActivity.java
+++ Chapter 3 Activites and Layout/ScrollApp/app/src/main/java/com/example/scrollapp/MainActivity.java
@@ -3,19 +3,24 @@ package com.example.scrollapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity{
-    Button button = findViewById(R.id.button);
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        Log.d("General","This Button is working properly");
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+//                Toast.makeText(MainActivity.this, "There is a click on this button", Toast.LENGTH_SHORT).show();
+                Log.d("Ayush","This Button is working properly");
             }
         });
     }
