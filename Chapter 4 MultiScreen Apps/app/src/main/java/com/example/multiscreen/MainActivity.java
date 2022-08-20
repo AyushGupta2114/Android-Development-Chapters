f908d7a118169990ec0f7b6a2a8febd7be70b1aa
ayush <ayushgupta220503@gmail.com>
2022-08-20 19:07:40 +0530

    multi screen using new mainActivity 2

--- Chapter 4 MultiScreen Apps/app/src/main/java/com/example/multiscreen/MainActivity.java
+++ Chapter 4 MultiScreen Apps/app/src/main/java/com/example/multiscreen/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.multiscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.DataOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void openActivity(View v){
+        Toast.makeText(this, "Opening Second Activity", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this,MainActivity2.class);
+        startActivity(intent);
     }
 }
