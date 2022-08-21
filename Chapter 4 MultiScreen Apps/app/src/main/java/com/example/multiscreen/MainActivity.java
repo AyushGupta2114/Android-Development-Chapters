1fead2fc50245567e7f51e6700944675e7b4fc10
ayush <ayushgupta220503@gmail.com>
2022-08-21 10:48:53 +0530

    First Multi Screen App😎

--- Chapter 4 MultiScreen Apps/app/src/main/java/com/example/multiscreen/MainActivity.java
+++ Chapter 4 MultiScreen Apps/app/src/main/java/com/example/multiscreen/MainActivity.java
@@ -12,17 +12,22 @@ import java.io.DataOutputStream;
 
 public class MainActivity extends AppCompatActivity {
     EditText name;
-    public static final String Extra_NAME = "com.example.multiscreen.extra.NAME";
+    public static final String Extra_NAMAyushE = "com.example.multiscreen.extra.NAME";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        name=findViewById(R.id.name);
-        String nameText = name.getText().toString();
+
+
     }
     public void openActivity(View v){
         Toast.makeText(this, "Opening Second Activity", Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(this,MainActivity2.class);
+        name=findViewById(R.id.name);
+        String nameText = name.getText().toString();
+        intent.putExtra(Extra_NAME,nameText);
         startActivity(intent);
+
+
     }
 }
