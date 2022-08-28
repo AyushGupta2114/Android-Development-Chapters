57bf0e43c49557039211b90dffc84d73a1ad9be1
ayush <ayushgupta220503@gmail.com>
2022-08-29 00:28:53 +0530

    built in XML layout

--- Chapter 5/app/src/main/java/com/example/listviewapp/AyushAdapter.java
+++ Chapter 5/app/src/main/java/com/example/listviewapp/AyushAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,8 +14,10 @@ import androidx.annotation.Nullable;
 public class AyushAdapter extends ArrayAdapter<String>{
 
     private String[] arr;
+    private Context context;
     public AyushAdapter(@NonNull Context context, int resource, @NonNull String[] arr) {
         super(context, resource, arr);
+        this.context=context;
         this.arr=arr;
     }
 
@@ -28,6 +31,13 @@ public class AyushAdapter extends ArrayAdapter<String>{
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         convertView= LayoutInflater.from(getContext()).inflate(R.layout.my_ayush_layout,parent,false);
         TextView t= convertView.findViewById(R.id.textView);
+        // Before returning the viw, add click list
+        convertView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(context, "You clicked on :"+position, Toast.LENGTH_SHORT).show();
+            }
+        });
         t.setText(getItem(position));
         return convertView;
     }
--- Chapter 5/app/src/main/java/com/example/listviewapp/MainActivity.java
+++ Chapter 5/app/src/main/java/com/example/listviewapp/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.listviewapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     ListView listview;
@@ -19,5 +22,12 @@ public class MainActivity extends AppCompatActivity {
 //        using built in array adapter
         AyushAdapter ad = new AyushAdapter(this,R.layout.my_ayush_layout,arr);
         listview.setAdapter(ad);
+        // listener on ArrayAdapter lis view
+//        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+//                Toast.makeText(MainActivity.this, "You clicked on :"+i, Toast.LENGTH_SHORT).show();
+//            }
+//        });
     }
 }
