0fb5739bfbef9c5250fdf40102561c0c7822b3db
AyushGupta2114 <ayushgupta220503@gmail.com>
2022-10-27 01:26:50 +0530

    new adapter code same as the recycle app of Contact Adapter just contact dictionary in place of string

---
+++ Chapter 5/DetailsContact/app/src/main/java/com/example/detailscontact/Contact.java
@@ -0,0 +1,15 @@
+package com.example.detailscontact;
+
+public class Contact {
+    private int sno;
+    private String phoneNumber;
+    private String name;
+
+    Contact(int sno,String phoneNumber,String name)
+    {
+        this.sno=sno;
+        this.phoneNumber=phoneNumber;
+        this.name=name;
+
+    }
+}
--- Chapter 5/DetailsContact/app/src/main/java/com/example/detailscontact/MainActivity.java
+++ Chapter 5/DetailsContact/app/src/main/java/com/example/detailscontact/MainActivity.java
@@ -5,7 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    Contact o1 =new Contact(1,"8765705001","Harry");
+    Contact o2 =new Contact(1,"9898989898","parry");
+    Contact o3 =new Contact(1,"4252233333","carry");
+    Contact o4 =new Contact(1,"7887878787","Hbarry");
+    Contact o5 =new Contact(1,"9898989898","thor");
+    Contact o6 =new Contact(1,"89898545454","Ayush");
+    Contact o7 =new Contact(1,"87878754545","caption");
+    Contact o8 =new Contact(1,"45424545415","Iron man");
+    Contact o9 =new Contact(1,"57542575755","mark zuker");
+    Contact o10 =new Contact(1,"5454875454","elon musk");
+    Contact o11 =new Contact(1,"9898989898","diana prince");
 
+    Contact [] contacts ={o1,o2,o3,o4,o5,o6,o7,o8,o9,o10,o11}
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
